ad81928b2da05cd78f310b2b87c0bdc7dec8c78d
Yaswanth Reddy Vari <81020845+yaswanth897@users.noreply.github.com>
2023-04-24 23:47:12 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,5 @@
 import pyrebase
-
-S_mail="yasurv05@gmail.com"
-S_password="Avengers@9440"
-firebaseConfig = {"apiKey": "AIzaSyAho6zWZZOuAuzxh2t_SHT2Ip6MrtZx6Rk",
-                  "authDomain": "automatic-no-parking-fine-sys.firebaseapp.com",
-                  "databaseURL": "https://automatic-no-parking-fine-sys-default-rtdb.firebaseio.com",
-                  "projectId": "automatic-no-parking-fine-sys",
-                  "storageBucket": "automatic-no-parking-fine-sys.appspot.com",
-                  "messagingSenderId": "206227731173",
-                  "appId": "1:206227731173:web:5ee8496a158ab0b369da0c",
-                  "measurementId": "G-1VSFMCX8GQ"
+firebaseConfig = {"Your Firebase Data"
                  }
 
 firebase=pyrebase.initialize_app(firebaseConfig)
